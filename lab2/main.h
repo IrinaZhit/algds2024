f88cc4df0f9b52cb12ce15c4b9a2a809dd65e23c
IrinaZhit <165552692+IrinaZhit@users.noreply.github.com>
2024-05-05 18:31:06 +0300

    Create main.h

---
+++ lab2/main.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifndef TREAP_H
+#define TREAP_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+Node* create(int key, int priority);
+
+Node* merge(Node* a, Node* b);
+
+Node* insert(Node* root, int key, int priority);
+
+Node* rotateRight(Node* y);
+
+Node* rotateLeft(Node* x);
+
+#endif
